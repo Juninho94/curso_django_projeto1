877bf40d000531a5cb951639ca7c7b59bef5f28f
Juninho94 <juninhosb2018@outlook.com>
2023-07-18 13:11:30 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -6,12 +6,7 @@ def home(request):
     return render(request, 'home.html')
 
 
-def contato(request):
-    return HttpResponse('contato')
 
-
-def sobre(request):
-    return HttpResponse('sobre')
 
   
 
